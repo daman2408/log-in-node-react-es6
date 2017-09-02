f035ea7e2e967e57ef78ed47e5f2defb8a63e3de
Daman <damanvir32@gmail.com>
2017-09-02 00:10:42 -0400

    building

--- gulpfile.js
+++ gulpfile.js
@@ -42,37 +42,6 @@ gulp.task('build', function() {
   .pipe(gulp.dest('./public/js/'))
 });
 
-gulp.task('babel', function() {
-  return gulp.src('./server/app.js')
-  .pipe(babel({
-    presets: ["react", "es2015", "stage-2"]
-  }))
-  .pipe(gulp.dest('./server'))
-})
-
-gulp.task('serve', function() {
-  // gulp.watch(['./**/*.{css, js, html}'], ['sort']);
-
-  //start the server
-  var server = gls('./server/app.js');
-  server.start()
-
-  //reload server when backend files change
-  gulp.watch(['./server/**/*.js'], function functionName() {
-    server.start.bind(server)();
-  });
-
-  //notify server when frontend files change
-  gulp.watch(['/.public/**/*.{css, js, html}'], function(file) {
-    server.notify(file)
-  })
-
-});
-
-gulp.task('start', function() {
-  return sequence('babel', 'serve')();
-})
-
 gulp.task('sort', function() {
   return sequence('clean:public','html', 'css','build')()
 });
